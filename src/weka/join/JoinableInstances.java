732362ebb168e66cfa8f188141b886cfa21c2665
btaranta <taranta.bogdan@gmail.com>
2015-02-06 15:15:30 +0100

    interface

---
+++ src/weka/join/JoinableInstances.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package weka.join;
+
+/**
+ * Description of new methods.
+ * @see weka.core.Instances
+ * @author Bogdan Taranta
+ *
+ */
+public interface JoinableInstances {
+//TBD
+}
